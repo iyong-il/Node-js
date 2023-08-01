cd80ccff60331ee38bb8c5586ce17768c2f5cd2b
iyong-il <lyi1233313@gmail.com>
2023-08-01 20:41:16 +0900

    기본문법공부

---
+++ Javascript-code/var, let, const/main.js
@@ -0,0 +1,24 @@
+// var greeting = "Hello World!";
+// console.log(greeting);
+
+// var greeting = "hi";
+// console.log(greeting);
+
+// let greeting1 = "hello";
+// console.log(greeting1);
+
+const greeting = "hello";
+// greeting = "2";
+console.log(greeting);
+
+const sss = () => {
+  if (true) {
+    var a = "a";
+    console.log(a);
+  }
+  console.log(a);
+};
+sss();
+
+// var 는 함수레벨
+// let, const는 블록레벨
